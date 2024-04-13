32e92b1900d56674834f7a0a15361ca558eac2db
alex <sytnikov.zp@gmail.com>
2024-04-14 00:13:59 +0300

    Fix index.css & disable 'open' option in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3005,
+    port: 3000,
     strictPort: true,
-    open: true,
+    // open: true,
   },
 });
